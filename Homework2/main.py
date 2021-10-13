0e76b919e1e94e9f02fc63d879d661dd10ff76e2
bryan119 <81392255+bryan119@users.noreply.github.com>
2021-10-13 17:44:26 -0500

    Part c.
    Pasted the existing contents in inputDates.txt into parsedDates.txt on line 17.

--- Homework2/main.py
+++ Homework2/main.py
@@ -1,7 +1,7 @@
 #Bryan Nguyen
 #1855265
 
-#Part b
+#Part c
 
 def find(datetime):
     months = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9,
@@ -13,8 +13,7 @@ def find(datetime):
     year = datetime.split()[-1]
     return format(m1) + str('/') + day + str('/') + year
 
-#Reading .txt file
-f = open('inputDates.txt', 'r')
-for line in f:
-    print(line, end='')
-f.close()
+#Reading and writing files
+with open('inputDates.txt', 'r') as file, open('parsedDates.txt', 'a') as line:
+    for i in file:
+        line.write(i)
