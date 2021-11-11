76e548767b77e0769106db0ae43735d962f8528b
bryan119 <81392255+bryan119@users.noreply.github.com>
2021-11-10 21:31:44 -0600

    Update Zylabs 10.11.py

--- Homework3/Zylabs 10.11.py
+++ Homework3/Zylabs 10.11.py
@@ -1,6 +1,7 @@
 #Bryan Nguyen
 #1855265
 
+#Created FoodItem class to initiate a constructor
 class FoodItem:
     def __init__(self, name = 'None', fat = 0.0, carbs = 0.0, protein = 0.0, servings = 0.0):
         self.name = name
@@ -9,18 +10,22 @@ class FoodItem:
         self.protein = protein
         self.servings = servings
 
+#Parameters to initalize attributes and calculate calories      
     def get_calories(self, num_servings):
         # Calorie formula
         calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings;
         return calories
-
+    
+#Prints the food item
     def print_info(self):
         print('Nutritional information per serving of {}:'.format(self.name))
         print('   Fat: {:.2f} g'.format(self.fat))
         print('   Carbohydrates: {:.2f} g'.format(self.carbs))
         print('   Protein: {:.2f} g'.format(self.protein))
 
+
 if __name__ == '__main__':
+#User input   
     food_name = input()
     fat = float(input())
     carbs = float(input())
